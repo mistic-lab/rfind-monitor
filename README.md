86f8cf57f5fb56fadd5277cc9a4034a0c92ee725
Nicholas Bruce <saltair93+github@gmail.com>
2021-03-14 21:31:44 -0700

    MWE with low res streaming

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # web-spectra-explorer
+
+## Goal
+
+Pull integrated spectra from endpoint and display spectrum, waterfall plot. Allow to select a playback speed start time. Then smoothly transition bandwidths/etc.
+
+This should all be running as an app and available by hitting some URL in modern browsers.
+
+## To start
+
+Same as above but without the endpoint. Simulate it for now.
+
+## Refs
+
+[1](https://community.plotly.com/t/heatmap-is-slow-for-large-data-arrays/21007/3) To do with image resampling when zooming in.
+
+[2](https://stackoverflow.com/questions/63589249/plotly-dash-display-real-time-data-in-smooth-animation) Intro to client-side callbacks and the extendData method.
+
+[3](https://community.plotly.com/t/heatmap-performance-layout-and-related-questions/26899) Seems like someone was having this exact problem with large images. Will need to look into shaders.
---
+++ app.py
@@ -0,0 +1,116 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+from dash.dependencies import Input, Output
+import h5py
+import numpy as np
+
+simDataFile = h5py.File('data.h5','r')
+spec = simDataFile['spec']
+
+waterfall_height = 200 # px
+
+
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div([
+    html.Div([
+        dcc.Input(id='refresh_rate_chooser', type='number', placeholder="1 second refresh rate", min=0.5, max=10, step=0.1),
+        dcc.Interval(id='refresh_rate', interval=1000), # ms
+    ]),
+    html.Div([
+        dcc.Graph(id='spec', responsive=True, config=dict(displayModeBar=False),
+            figure={
+                'data': [{
+                    'type': 'line',
+                    'y': spec[0],
+                    
+                }],
+                'layout': {
+                    'width': '100%',
+                    'height': '100%',
+                    'margin': {
+                        't': 0,
+                        'b':0,
+                        'r':0,
+                        'l':0,
+                    },
+                    'yaxis': {
+                        'range': [0,60]
+                    },
+                }
+            },
+            style={'height': '20vh'}
+        ),
+        dcc.Graph(id='waterfall', responsive=True, config=dict(displayModeBar=False, doubleClick='reset'), 
+            figure={
+                'data': [{
+                    'type': 'heatmapgl', #* heatmapgl is also available but double clicking doesn't reset to home?
+                    'z': spec[0:waterfall_height],
+                    # 'aspect': 'equal',
+                    'colorbar': {
+                        'len': 0.5,
+                        'x': 0.95,
+                        'y': 0.95,
+                        'yanchor': 'top'
+                    },
+                }],
+                'layout': {
+                    'width': '100%',
+                    'height': '100%',
+                    'margin': {
+                        't': 0,
+                        'b': 0,
+                        'r':0,
+                        'l':0,
+                    },
+                }
+            }
+        )
+    ]),
+],
+style={'height':'100vh'}
+)
+
+
+## Callbacks 
+@app.callback(
+    Output("refresh_rate", "interval"),
+    [Input("refresh_rate_chooser", "value")], prevent_initial_call=True)
+def update_refresh_rate(rate):
+    return rate*1000
+
+@app.callback([
+    Output("spec", "extendData"),
+    Output("waterfall", "extendData")
+], [Input("refresh_rate", "n_intervals")], prevent_initial_call=True)
+def update_spec(index):
+    newSpec = spec[index:index+waterfall_height]
+    newLine = spec[index]
+    updatedSpec = dict(y=[newLine]), [0], spec.shape[1]
+    updatedWaterfall = dict(z=[newSpec]),[0], waterfall_height
+
+    #TODO This would be better since it would not need a full new matrix each time
+    # newSpec = spec[index] 
+    # updatedSpec = dict(y=[newSpec]), [0], spec.shape[1]
+    # updatedWaterfall = dict(z=[newSpec]),[0], waterfall_height
+
+    return updatedSpec, updatedWaterfall
+
+#TODO use relayoutData from zoom event on waterfall to update xaxis on spec
+#TODO https://dash.plotly.com/interactive-graphing
+# @app.callback(
+#     Output("spec","relayoutData"),
+#     [Input("waterfall","relayoutData")], prevent_initial_call=True)
+# def update_spec_xaxis(relayOutData):
+
+
+
+
+
+
+app.run_server(debug=True)
+
+simDataFile.close()
